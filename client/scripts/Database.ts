631b620b472cde7c9536e92882067ed0309024f1
Malloc of Kuzkycyziklistan <aleco@aroltd.com>
2017-04-17 13:50:18 -0500

    first
    worst

---
+++ client/scripts/Database.ts
@@ -0,0 +1,5 @@
+class FileCache {
+    static Initialize() {
+        
+    }
+}
---
+++ server/Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace CircleScape {
+    class Server : WebSocketBehavior {
+        Server() {
+            
+        }
+
+        protected override void OnOpen() {
+
+        }
+
+        protected override void OnMessage(MessageEventArgs e) {
+            
+        }
+
+        protected override void OnClose(CloseEventArgs e) {
+            
+        }
+    }
+
+    class EntryPoint {
+        static void Main(string[] args) {
+
+        }
+    }
+}
