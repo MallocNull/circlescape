91c4ce5360ce7d4e119c22f2b8292d01d76a9358
Malloc of Kuzkycyziklistan <aleco@aroltd.com>
2017-04-21 16:04:03 -0500

    fuck websocketsharp
    time to die

---
+++ server/Websocket/Pool.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CircleScape.Websocket {
+    class Pool {
+        private const int InitialCount = 3;
+        private const int InitialSize  = 3;
+        private const int SizeGrowth   = 1;
+        private const int MaxSize      = 10;
+
+        private int _fullThreadCount;
+        private bool updateFullThreadCount = true;
+
+        private List<ThreadContext> Threads
+            = new List<ThreadContext>();
+        private Dictionary<UInt64, Connection> Connections
+            = new Dictionary<UInt64, Connection>();
+
+        public Pool() {
+            for(var i = 0; i < InitialCount; ++i) {
+                
+            }
+        }
+
+        public bool AddConnection(Connection connection) {
+            foreach(var thread in Threads) {
+
+            }
+        }
+
+        private ThreadContext CreateThread(Connection initialConnection = null) {
+            var stack = new Stack(true);
+            var ctx = new ThreadContext {
+                Stack = stack,
+                Thread = new Thread(new ThreadStart(stack.ManageStack))
+            };
+
+            Threads.Add(ctx);
+            return ctx;
+        }
+
+        private int FullThreadCount {
+            get {
+                if(updateFullThreadCount) {
+                    _fullThreadCount = Math.Min(
+                            MaxSize, 
+                            InitialSize + SizeGrowth * (Threads.Count - InitialCount)
+                        );
+                }
+
+                return _fullThreadCount;
+            }
+        }
+
+        class ThreadContext {
+            public Thread Thread { get; set; }
+            public Stack Stack { get; set; }
+        }
+    }
+}
---
+++ server/Websocket/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace CircleScape.Websocket {
+    class Stack {
+        private List<Connection> Clients = new List<Connection>();
+        private Mutex ClientsMutex = new Mutex();
+        private bool RunWithNoClients = false;
+
+        public Stack(Connection initialConnection = null) {
+            if(initialConnection != null)
+                Clients.Add(initialConnection);
+        }
+
+        public Stack(bool runWithNoClients, Connection initialConnection = null)
+            : this(initialConnection) 
+        {
+            RunWithNoClients = runWithNoClients;
+        }
+
+        public bool AddClient(Connection client) {
+            if(!ClientsMutex.WaitOne(5000))
+                return false;
+
+            Clients.Add(client);
+
+            ClientsMutex.ReleaseMutex();
+            return true;
+        }
+
+        public int ClientCount {
+            get {
+                return Clients.Count;
+            }
+        }
+
+        // USED FOR THREADING -- DO NOT CALL
+        public void ManageStack() {
+            int clientCount = ClientCount;
+
+            for(var i = 0; i < clientCount; ++i)
+        }
+    }
+}
