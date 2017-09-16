952018c018d510b1f5b74a600c770f4a8fd24a2c
Malloc of Kuzkycyziklistan <aleco@aroltd.com>
2017-09-15 19:08:09 -0500

    AAAAAAAAAAAAAAAAAA
    forgot

--- client/build.bat
+++ client/build.bat
@@ -1,5 +1,5 @@
 @echo off
 del lib.js > nul 2>&1
 copy lib\*.js /a lib-raw.js /b > nul 2>&1
-cmd /C "uglifyjs lib-raw.js -o bin\lib.js"
+cmd /C "uglifyjs lib-raw.js -o js\lib.js"
 del lib-raw.js > nul 2>&1
