470bf21c436615a4ba4597e4da98f29d2057618b
malloc <aleco@aroltd.com>
2018-04-11 16:28:00 -0500

    flat pool

--- server/src/sock/pool.hpp
+++ server/src/sock/pool.hpp
@@ -1,6 +1,11 @@
 #ifndef SOSC_POOL_H
 #define SOSC_POOL_H
 
+#include <vector>
+#include <list>
+#include <thread>
+#include <mutex>
+
 namespace sosc {
 template<typename T>
 class Pool {
@@ -9,7 +14,21 @@ public:
 protected:
     virtual void ProcessClient(T* client) = 0;
 private:
+    class Stack {
+    public:
+        
+    private:
+        std::thread thread;
+        std::list<T*> clients;
+        std::mutex clients_mtx;
+        Pool<T> *pool;
+        
+        friend class Pool<T>;
+    };
     
+    std::vector<Stack> stacks;
+    
+    friend class Stack;
 };
 }
 
