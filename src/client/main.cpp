f722ee0cf60f441315ff823da41120798bb1c37c
MallocNull <aleco@aroltd.com>
2018-09-14 11:37:59 -0500

    scoop boob

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -18,8 +18,8 @@ void draw();
 int main(int argc, char** argv) {
     using namespace sosc;
 
-    /*if(argc != 3)
-        return -1;*/
+    if(argc != 3)
+        return -1;
 
     int client_width  = std::stoi(argv[1]),
         client_height = std::stoi(argv[2]);
@@ -50,6 +50,8 @@ int main(int argc, char** argv) {
     if(glewInit() != GLEW_OK)
         return -1;
 
+    EM_ASM(setup_resize_event());
+
     ui::font_init_subsystem(_ctx.window);
     _ctx.font = new ui::Font(
         SOSC_RESC("fonts/scape.bmp"),
@@ -64,14 +66,40 @@ int main(int argc, char** argv) {
 
     emscripten_set_main_loop(draw, 0, 1);
 
-    std::cout << "end test" << std::endl;
+    _ctx.font->Unload();
+    _ctx.text->Destroy();
+
+    ui::font_deinit_subsystem();
+    SDL_GL_DeleteContext(_ctx.gl_ctx);
+    SDL_DestroyWindow(_ctx.window);
+
+    return 0;
 }
 
 void draw() {
+    using namespace sosc;
+
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(.25, .25, .25, 1);
 
     _ctx.text->Render();
 
     SDL_GL_SwapWindow(_ctx.window);
+
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        std::cout << "an event" << std::endl;
+
+        if(event.type == SDL_WINDOWEVENT &&
+           event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+        {
+            glViewport(0, 0, event.window.data1, event.window.data2);
+            ui::font_window_changed(_ctx.window);
+        }
+    }
+}
+
+extern "C"
+void resize_context(int width, int height) {
+    SDL_SetWindowSize(_ctx.window, width, height);
 }
